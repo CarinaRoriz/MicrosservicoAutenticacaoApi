244be1788adc81fde524799e1435b92e1fba3f4e
578931 <578931@PPL4INFLB140222>
2019-04-11 22:14:49 -0300

    Criação e configuração da UsuarioController

---
+++ MicrosservicoAutenticacaoApi/Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MicrosservicoAutenticacaoApi.Modelos;
+
+namespace MicrosservicoAutenticacaoApi.Controllers
+{
+    [Route("v1/usuarios")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        List<Usuario> listaUsuarios;
+
+        public UsuarioController()
+        {
+            listaUsuarios = new List<Usuario>
+            {
+                new Usuario { Id = 1, Nome = "Carina" },
+                new Usuario { Id = 2, Nome = "Renato" }
+            };
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<bool>> ValidaUsuario(long id)
+        {
+            var usuario = listaUsuarios.Where(l => l.Id == id).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return true;
+        }
+    }
+}
---
+++ MicrosservicoAutenticacaoApi/Modelos/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicrosservicoAutenticacaoApi.Modelos
+{
+    public class Usuario
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
